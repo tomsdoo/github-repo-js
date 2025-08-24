0fc63cc08889c7c6f86e58c7670a5bd9cb9c5224
tomsdoo <toms@live.jp>
2025-08-24 11:50:53 +0900

    getReferences

--- src/modules/repository.ts
+++ src/modules/repository.ts
@@ -3,6 +3,7 @@ import { readyGitHubIssue } from "@/modules/issue";
 import { readyGitHubRepositoryBranch } from "@/modules/repository-branch";
 import { readyGitHubRepositoryBranches } from "@/modules/repository-branches";
 import { readyGitHubRepositoryIssues } from "@/modules/repository-issues";
+import { readyGitHubRepositoryReferences } from "@/modules/repository-references";
 import type { Endpoints } from "@octokit/types";
 
 export function readyGitHubRepository(token: string) {
@@ -10,6 +11,7 @@ export function readyGitHubRepository(token: string) {
   const GitHubRepositoryIssues = readyGitHubRepositoryIssues(token);
   const GitHubRepositoryBranch = readyGitHubRepositoryBranch(token);
   const GitHubRepositoryBranches = readyGitHubRepositoryBranches(token);
+  const GitHubRepositoryReferences = readyGitHubRepositoryReferences(token);
   return class extends GitHubRepoApiBase<
     Endpoints["GET /repos/{owner}/{repo}"]["response"]["data"],
     Endpoints["PATCH /repos/{owner}/{repo}"]["request"]["data"]
@@ -25,6 +27,15 @@ export function readyGitHubRepository(token: string) {
     }
     async getBranch(branchName: string) {
       return await new GitHubRepositoryBranch(this.owner, this.repo, branchName).get();
+    }
+    async getReferences(refName: string) {
+      return await new GitHubRepositoryReferences(this.owner, this.repo).getMatchingList(refName);
+    }
+    async getBranchReferences() {
+      return await new GitHubRepositoryReferences(this.owner, this.repo).getMatchingList("heads/");
+    }
+    async getTagReferences() {
+      return await new GitHubRepositoryReferences(this.owner, this.repo).getMatchingList("tags/");
     }
     async getIssues() {
       return await new GitHubRepositoryIssues(this.owner, this.repo).getList();
