edb85f6681df2770523dfb63db6df4dfdf41e2b4
tomsdoo <32782986+tomsdoo@users.noreply.github.com>
2025-08-24 11:51:21 +0900

    getReferences (#3)

---
+++ src/modules/repository-references.ts
@@ -0,0 +1,32 @@
+import { GitHubRepoApiBase } from "@/modules/base";
+import type { Endpoints } from "@octokit/types";
+
+export function readyGitHubRepositoryReferences(token: string) {
+  class GitHubRepositoryMatchingReferences extends GitHubRepoApiBase<
+    Endpoints["GET /repos/{owner}/{repo}/git/matching-refs/{ref}"]["response"]["data"] extends (infer T)[] ? T : never,
+    unknown
+  > {
+    protected refName: string;
+    constructor(owner: string, repo: string, refName: string) {
+      super(token, owner, repo);
+      this.refName = refName;
+    }
+    get apiEndpoint() {
+      return `${this.apiOrigin}/repos/${this.owner}/${this.repo}/git/matching-refs/${this.refName}`;
+    }
+  }
+  return class extends GitHubRepoApiBase<
+    Endpoints["GET /repos/{owner}/{repo}/git/matching-refs/{ref}"]["response"]["data"] extends (infer T)[] ? T : never,
+    Endpoints["POST /repos/{owner}/{repo}/git/refs"]["request"]["data"]
+  > {
+    constructor(owner: string, repo: string) {
+      super(token, owner, repo);
+    }
+    get apiEndpoint() {
+      return `${this.apiOrigin}/repos/${this.owner}/${this.repo}/git/refs`;
+    }
+    async getMatchingList(refName: string) {
+      return await new GitHubRepositoryMatchingReferences(this.owner, this.repo, refName).getList();
+    }
+  };
+}
