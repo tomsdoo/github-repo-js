baa1a26e08e7a9ef13c2c1e79fc2614d3820dca2
tomsdoo <32782986+tomsdoo@users.noreply.github.com>
2025-08-26 08:51:39 +0900

    getReleases (#10)

---
+++ src/modules/repository-releases.ts
@@ -0,0 +1,22 @@
+import { GitHubRepoApiBase } from "@/modules/base";
+import type { Endpoints } from "@octokit/types";
+
+export function readyGitHubRepositoryReleases(token: string) {
+  return class extends GitHubRepoApiBase<
+    Endpoints["GET /repos/{owner}/{repo}/releases"]["response"]["data"] extends (infer T)[] ? T : never,
+    Endpoints["POST /repos/{owner}/{repo}/releases"]["request"]["data"]
+  > {
+    constructor(owner: string, repo: string) {
+      super(token, owner, repo);
+    }
+    get apiEndpoint() {
+      return `${this.apiOrigin}/repos/${this.owner}/${this.repo}/releases`;
+    }
+    get pageSizeForRequest() {
+      return 100;
+    }
+    async getList(query?: Record<string, string>) {
+      return await this.getPagedList(query);
+    }
+  };
+}
